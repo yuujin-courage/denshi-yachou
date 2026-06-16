88f1d087abc3cc0b64fe248f1206ded541e14bfe
yuujin-courage <mochiatu0325@gmail.com>
2026-06-16 13:22:08 +0900

    SW完全無効化・キャッシュ無効化ヘッダー追加

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-const CACHE_NAME = 'denshi-yachou-v5';
-
-self.addEventListener('install', () => {
+self.addEventListener("install", function() {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", function(event) {
   event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.map(function(k) { return caches.delete(k); })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
   );
 });
